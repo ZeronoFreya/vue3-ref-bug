5be3c66a94380a8e25b2ffa096ed6c5430471de1
zeronofreya <zeronofreya@gmail.com>
2021-03-29 17:14:51 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>demo</div>
+</template>
+
+<script setup>
+ref: demo = "";
+
+const temp1 = {
+  demo,
+};
+console.log(temp1);   // output: {demo: ""}
+
+const fun = () => {
+  const temp2 = {
+    demo,
+  };
+  console.log(temp2);  // output: {demo: RefImpl}
+};
+fun();
+</script>
